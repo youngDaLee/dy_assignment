ce10731af8050c600aa06c40b282491812a22fab
YoungDaLee <ldy0956@gmail.com>
2022-08-24 22:08:56 +0900

    Feat : Solve Calender Problem

--- Q01_Calendar_Problem.py
+++ Q01_Calendar_Problem.py
@@ -25,7 +25,6 @@ def add_leap(year, month):
     1
     """
     if (month == 2) and ((year%4==0 and year%100 !=0) or (year%400==0)):
-        print(year, month)
         return 1
     else:
         return 0
